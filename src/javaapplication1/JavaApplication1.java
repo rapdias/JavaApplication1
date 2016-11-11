bdfd6f1eb8509e43744e5cc712fbd64e98039d92
rogeriodias <rogeriodias@10.39.52.102>
2016-11-11 08:46:15 -0200

    ffddfd

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rogeriodias
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showConfirmDialog(null, "ÖK");
+    }
+    
+}
