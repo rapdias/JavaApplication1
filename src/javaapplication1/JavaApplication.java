eb1b26fe75723155d51a03c063711611e55535d9
rogeriodias <rogeriodias@10.39.52.102>
2017-01-05 21:30:22 -0200

    comit inicial

--- src/javaapplication1/JavaApplication.java
+++ src/javaapplication1/JavaApplication.java
@@ -6,19 +6,19 @@
 package javaapplication1;
 
 import javax.swing.JOptionPane;
-
+import testelib.TesteLib;
 /**
  *
  * @author rogeriodias
  */
-public class JavaApplication1 {
+public class JavaApplication {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JOptionPane.showConfirmDialog(null, "ÖK");
+        JOptionPane.showConfirmDialog(null, new TesteLib().msg());
     }
     
 }
